9f6538a797417a977cd132998db07cd5b4b14a17
Rafael Antonio <rafael.lopez@karimnot.com>
2023-02-19 21:49:56 -0600

    repositorio creado

---
+++ src/main/java/com/ulsa/main/ETechProjectApplication.java
@@ -0,0 +1,14 @@
+package com.ulsa.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETechProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETechProjectApplication.class, args);
+		System.out.println("Tienda en línea y estable.");
+	}
+
+}
---
+++ src/test/java/com/ulsa/demo/ETechProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulsa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETechProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
